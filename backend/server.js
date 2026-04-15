0caf58e196a6f09009bf56fe037cd0d38df1a177
santhosh2362003 <santhoshraj2362003@gmail.com>
2026-04-15 09:22:36 +0530

    Update index.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 const app = express();
-app.use(cors());
 
-mongoose.connect(process.env.DB_URL || 'mongodb://db:27017/mytask');
+app.use(express.json());
 
-app.get('/api/status', (req, res) => {
-    res.json({ message: "Backend & MongoDB Connected Successfully!" });
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("DB Connected"))
+.catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send("Backend Working 🚀");
 });
 
-app.listen(5000, () => console.log('Server running on 5000'));
+app.listen(5000, () => console.log("Server running on 5000"));
